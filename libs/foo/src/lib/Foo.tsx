c458c7eec9beff4a12b5e09ff7113a9fbf6fb991
Mike Chabot <mchabot@draftkings.com>
2020-10-29 17:29:38 -0400

    Add bar to foo

--- libs/foo/src/lib/Foo.tsx
+++ libs/foo/src/lib/Foo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import {Bar} from "@my-test-org/bar";
 
 /* eslint-disable-next-line */
 export interface FooProps {}
@@ -13,6 +14,7 @@ export const Foo = (props: FooProps) => {
   return (
     <StyledFoo>
       <h1>Welcome to foo!</h1>
+      <Bar />
     </StyledFoo>
   );
 };
